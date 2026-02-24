4233b796ca9448f921228e0ad3436c092a11657e
Joao <joaoluiscostaferreira7@gmail.com>
2026-02-24 20:08:35 -0300

    I1

---
+++ src/main/java/org/example/ContaBancariaRepository.java
@@ -0,0 +1,15 @@
+package org.example.repositories;
+
+import org.example.models.ContaBancaria;
+import java.utill.ArrayList;
+import java.utill.list;
+
+public class ContaBancariaRepository {
+public ContaBancaria save (ContaBancaria contaBancaria){
+    return new ContaBancaria();
+  }
+public List<ContaBancaria> findAll(){
+    return new Arraylist<>();
+  }
+}
+
